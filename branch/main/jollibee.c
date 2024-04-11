386ea7c3eebeebb46ada40e994305108c2dd3e11
J2 <jsonbartolay@gmail.com>
2024-04-11 15:14:20 +0800

    aprl 11 chnges

--- branch/main/jollibee.c
+++ branch/main/jollibee.c
@@ -10,32 +10,28 @@ int subtotal[4] = {0, 0, 0, 0};
 int price [4] = {0, 25, 30, 15};
 int stocks [4] = {0, 100, 100, 100};
 int quantity [4] = {0, 0, 0, 0};
-int sub_sales [4] = {0, 0, 0, 0};
 int total_sales [4] = {0, 0, 0, 0};
-int present_stocks [4] = {0, 100, 100, 100};
-int present_quantity [4] = {0, 0, 0, 0};
-int stock_beginning [4] = {0, 100, 100, 100};
-char description [5][10] = {"","Hamburger", "French", "Coke", "Exit"}; 
+char description [5][10] = {"","Hamburger", "Fries", "Coke", "Exit"}; 
 
-//! none array global variables
+//! none array global variablesxN
 int num;
 char ans;
 int may_order = 0;
 int col, row;
-int order_no = 1;
+int order_no = 1;   
 int choice;
 int total_bill = 0; 
-int stock_after = 0; 
 int grand_total = 0;
-int stock_before = 0;
 float payment, change;
 
-void gotoxy (int x, int y){
-    COORD coord;
-    coord.X = x - 1;
-    coord.Y = y - 1;
-    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+void gotoxy(int x,int y){
+COORD coord;
+coord.X = x-1;
+coord.Y = y-1;
+SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE),coord);
+
 }
+
 
 int box (int col1, int col2, int row1, int row2){ //!function para sa box
     for(col=col1;col<=col2;col++){
@@ -104,6 +100,7 @@ void enter_order (){//! 70% of process is stored in this function
     goto_sang_cancel_order: 
     main_menu (); //! call out and function
     order: //! unecessary but incase
+    g(2, 9);p("                  "); 
     g(2, 8);p("Enter Choice: "); //! prompt to enter a choice
     for (num = 1; num > 0; num++){ //! loop for entering choice endless, ma end lang if mag enter choice '4' which is exit
         g(17, 8);s("%d", &choice);
@@ -115,15 +112,17 @@ void enter_order (){//! 70% of process is stored in this function
             g(25, 8);s("%d", &quantity[choice]);
             if (quantity [choice] <= stocks[choice]){ //! condition/ sentinel kung sufficient ang stocks sa quantity nya nga gin order
                 if (quantity[choice] > 0) { //! greater than 0 means may quantiy sya nga gin butang
-                    g(2, 9);p("              "); //! erase the invalid input display
+                    g(2, 9);p("                  "); //! erase the invalid input display
+                    
                     stocks[choice] = stocks[choice] - quantity[choice]; //! bawasan ang stocks
                     subtotal[choice] = price[choice] * quantity[choice];  //! e total iya balayran
                     total_sales[choice] = total_sales[choice] + subtotal[choice]; //! para  madisplay ang sales
                     total_bill = total_bill + subtotal[choice]; //!final nga balayran
                     total_sold [choice] = total_sold[choice] + quantity[choice]; //! para ma monitor ang quantiy sa kada item
+                    
                     g(40, 1);p("Item#");
                     g(47, 1);p("Description");
-                    g(60, 1);p("Price", price[choice]);
+                    g(60, 1);p("Price", price[choice]); 
                     g(68, 1);p("Qty", quantity[choice]);
                     g(72, 1);p("subtotal");
                     g(40, 2 + num);p("%d", num);
@@ -131,6 +130,7 @@ void enter_order (){//! 70% of process is stored in this function
                     g(60, 2 + num);p("P %d", price[choice]);
                     g(69, 2 + num);p("%d", quantity[choice]);
                     g(73, 2 + num);p("%d", subtotal[choice]);
+
                     g(17, 8);p("                    "); //! erase ang input sa pag iterate
                     g(32, 4);p("%d ", stocks[1]);
                     g(32, 5);p("%d ", stocks[2]);
@@ -142,10 +142,11 @@ void enter_order (){//! 70% of process is stored in this function
                 }
             } else { //! CONDITION PARA SA INVALID QUANTITY which is less than 0
                 num = num - 1;
-                g(2, 9);p("INVALID QUANTITY");
-                g(17, 8);p("                "); 
+                g(2, 9);p("Insufficient  ");
+                g(17, 8);p("                 "); 
             }
         }     
+
         else if (choice == 4){ //! ma proceed sa payment or kung mag exit without order ma ask another customer
             if (may_order == 1){//! nag set ta flag sa enter choice 1,2,3 if mag enter sa either sa tatlo ma set ang flag sa 1 means may order sya if 0 means wala sya order
                 pay:
@@ -183,15 +184,14 @@ void enter_order (){//! 70% of process is stored in this function
                     } else {
                         g(70, 4 + num);p("INVALID INPUT"); //! invalid input sang cancel order
                     }
-                } else {
-                    g(65, 5 + num);p("INVALID INPUT");//! invalid input sang payment
-                }
-            } else { //! else sang flag means if flag = 0 man gihapon wala sa order kag ma another customer nalng
-                order_no = order_no - 1;
+                } 
+
+            } else { //! else sang flag means if may_order = 0 man gihapon wala sa order kag ma another customer nalng
+                order_no = order_no - 1; // DECREMENT SANG ORDER IF MAG ERROR
                 break; //! ma break sya sa enter_order function directly ma ask another customer
             }
         } else {
-            g(2, 9);p("INVALID INPUT"); //! invalid input sa pag enter sang choice
+            g(2, 9);p("INVALID CHOICE"); //! invalid input sa pag enter sang choice
             num = num - 1; //! enter choice that is invalid is counted in loop, para ma prevent ang value sang loop kung error e decrement naton
             g(17, 8);p("              "); //! tamper sa display nga choice
         }   
@@ -205,13 +205,13 @@ int main (){
         enter_order(); //? sa enter order naga increment ang num para ma adjust depend sa kadamoun sang order ang another customer
         another_customer:
         g(50, 5 + num);p("Another Customer? "); //? ang increment sang num halin sa function nga enter_order();
-        g(67, 5 + num);s("%s", &ans);
+        g(67, 5 + num);s("%s", &ans); 
         if (ans == 'n' || ans == 'N') {
             box (1, 75, 15, 22 );//! we need new variable for incrementing and move the box
             display_total_inventory();
             g(55, 6 + num);p("Exit? ");
             g(70, 6 + num);s("%s", &ans);
-            if (ans == 'Y' || ans == 'y') { 
+            if (ans == 'Y' || ans == 'y') { //exit ans
                 break; //! ma break nasa sa while loop
             } else {
                 g(55, 6 + num);p("                   ");
